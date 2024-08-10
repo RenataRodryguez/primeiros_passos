09dddc6c14d9bc748d69f9b934a5776ccb1feda9
Renata <renata.rodriguesdasilva@outlook.com>
2024-08-09 22:26:10 -0300

    Qual é tipo ?

--- Exc3.py
+++ Exc3.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um número '))
 n2 = int(input('Digite outro número '))
 resultado = n1 + n2
-print(f'A soma de {n1} + {n2}, é: {resultado}. correto?')
+print(f'A soma de {n1} + {n2}, é: {resultado}. Correto?')
