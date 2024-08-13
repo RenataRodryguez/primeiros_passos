f8826b9696ff166f8581bc4d0478aa56e105b745
Renata <renata.rodriguesdasilva@outlook.com>
2024-08-13 20:07:35 -0300

    conversor de dolar

--- Exc10.py
+++ Exc10.py
@@ -1,3 +1,3 @@
-real = float(input('Quanto de dinheiro você tem na carteira? R$'))
+real = float(input('Quanto de dinheiro você tem na carteira? R$:'))
 dolar = real / 5.46
-print(f'Com {real:.2f} voce pode comprar US${dolar:.2f}')
+print(f'Com R${real:.2f}, você pode comprar US${dolar:.2f}')
