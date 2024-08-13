5176725b5b7f05dfae64a4e453a0726f351996fe
Renata <renata.rodriguesdasilva@outlook.com>
2024-08-12 23:12:22 -0300

    Media de notas

---
+++ Exc7.py
@@ -0,0 +1,5 @@
+aluno1 = float(input('Digite a nota do aluno 1:'))
+aluno2 = float(input('Digite a nota do aluno 2:'))
+soma = aluno1 + aluno2
+media = soma / 2
+print(f'A média entre {aluno1} e {aluno2} é {media}')
